626fb4dd2343c3f2cd7422cc9b656e28fbd15190
Aliaksei Khvarastouski <alexey_hw@tut.by>
2021-05-02 22:19:01 +0300

    [Step 3.1] Simulate rain drop growth

--- main.py
+++ main.py
@@ -8,6 +8,30 @@ DROP_INCREASE = 5
 RESET_SIZE = 50
 CANVAS_SIZE = 1000
 
+
+def draw_drops(canvas, rain_drops):
+    canvas.delete("all")
+    for i in range(0, WINDOW_SIZE):
+        for j in range(0, WINDOW_SIZE):
+            if random.random() <= RAIN_RATE:
+                rain_drops[i][j] += 1
+
+    coordinate_step = int(CANVAS_SIZE / (WINDOW_SIZE + 1))
+
+    for i in range(0, CANVAS_SIZE, coordinate_step):
+        canvas.create_line(i, 0, i, CANVAS_SIZE)
+        canvas.create_line(0, i, CANVAS_SIZE, i)
+
+    for i in range(0, WINDOW_SIZE):
+        for j in range(0, WINDOW_SIZE):
+            oval_y = (i + 1) * coordinate_step
+            oval_x = (j + 1) * coordinate_step
+            drop_size = rain_drops[i][j] * DROP_INCREASE
+            canvas.create_oval(oval_y - drop_size, oval_x - drop_size, oval_y + drop_size, oval_x + drop_size)
+
+    canvas.after(500, lambda: draw_drops(canvas, rain_drops))
+
+
 if __name__ == '__main__':
     main_window = tkinter.Tk()
     main_window.geometry("1000x1000")
@@ -16,20 +40,6 @@ if __name__ == '__main__':
     canvas = tkinter.Canvas(main_window)
     canvas.pack(fill=tkinter.BOTH, expand=tkinter.YES)
 
-    coordinate_step = int(CANVAS_SIZE / (WINDOW_SIZE + 1))
-
-    for i in range(0, 1000, coordinate_step):
-        canvas.create_line(i, 0, i, 1000)
-        canvas.create_line(0, i, 1000, i)
-
-    rain_drops = [[random.randint(1, 4) for j in range(0, 20)] for i in range(0, 20)]
-
-    for i in range(0, 20):
-        for j in range(0, 20):
-            oval_y = (i+1) * coordinate_step
-            oval_x = (j+1) * coordinate_step
-            drop_size = rain_drops[i][j] * DROP_INCREASE
-            canvas.create_oval(oval_y - drop_size, oval_x - drop_size, oval_y + drop_size, oval_x + drop_size)
-
+    rain_drops = [[0 for j in range(0, WINDOW_SIZE)] for i in range(0, WINDOW_SIZE)]
+    draw_drops(canvas, rain_drops)
     main_window.mainloop()
-
